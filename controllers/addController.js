f2a72369a234f6fee6f8f82e995967eaa87314e3
marekwino23 <marekw1996@gmail.com>
2021-05-27 09:48:02 +0200

    fix endpoints

--- controllers/addController.js
+++ controllers/addController.js
@@ -4,7 +4,8 @@ const db = require('../config/database');
  const addController = function addTask(req, res) {
     const task = req.body.text
     const date = req.body.date
-    db.query(`INSERT INTO Tasks(task_content, date) VALUES(?, ?)`,[task,date], function (err, rows, fields) {
+    const time = req.body.clock
+    db.query(`INSERT INTO Tasks(task_content, date, time) VALUES(?, ?, ?)`,[task,date,time], function (err, rows, fields) {
         if (err) {
             console.error("error")
         } else {
--- controllers/updateController.js
+++ controllers/updateController.js
@@ -9,7 +9,7 @@ const updateController = function updateTask (req, res) {
     const status = req.body.status
     const date = req.body.date
     console.log(id,content,date, time,status)
-    db.query(`Update Tasks set task_content=?, date=?, time=?, status = ? where id = ?`,[content,date,time,status,id], function (err, rows, fields) {
+    db.query(`Update Tasks set task_content=?, date=?, time=?, status = ? where id = ?`,[content,date, time, status, id], function (err, rows, fields) {
         if (err) {
             console.error("error")
         } else {
